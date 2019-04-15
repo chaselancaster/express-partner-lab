1eeb44c380ddad7e01838209c2097358bef84a89
chaselancaster <chaseblancaster@gmail.com>
2019-04-15 11:32:31 -0700

    Update server.js with variables and middleware

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+
+// Middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+
+app.listen(3000, () => {
+    console.log('app listening on port: ' 3000)
+});
