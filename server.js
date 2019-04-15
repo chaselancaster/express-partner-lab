2cfa7148d7a66af524118584fd4af12e82c9a982
chaselancaster <chaseblancaster@gmail.com>
2019-04-15 11:47:04 -0700

    Add index.ejs

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const app = express();
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 
-const Video = require('./models/Videogames');
+const Video = require("./models/Videogames");
 
 // Middleware
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+app.get("/videogames", (req, res) => {
+  res.render("index.ejs", {
+    vg: Video
+  });
+});
 
 app.listen(3000, () => {
-    console.log('app listening on port: ' 3000)
+  console.log("app listening on port: 3000");
 });
